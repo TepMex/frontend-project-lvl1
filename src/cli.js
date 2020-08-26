509a4bba99a238b6fae45df43010bde79da2c885
Mikhail Semikolenov <mihawww@yandex.ru>
2020-08-26 14:21:50 +0600

    asking for user name

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const greetingUser = (name) => console.log(`Hello, ${name}!`);
+
+export const askForName = () => {
+	const name = readlineSync.question('May I have your name? ');
+	greetingUser(name);
+};
