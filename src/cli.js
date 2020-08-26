b374b9c4b7c4b14cbb78df9a3feed507d059cd4c
Mikhail Semikolenov <mihawww@yandex.ru>
2020-08-26 15:05:12 +0600

    eslint added and configured

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,11 @@ import readlineSync from 'readline-sync';
 const greetingUser = (name) => console.log(`Hello, ${name}!`);
 
 export const askForName = () => {
-	const name = readlineSync.question('May I have your name? ');
-	greetingUser(name);
+  const name = readlineSync.question('May I have your name? ');
+  greetingUser(name);
+};
+
+export default {
+  greetingUser,
+  askForName,
 };
